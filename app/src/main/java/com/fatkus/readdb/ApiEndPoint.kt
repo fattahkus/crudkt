57dd3de77b7e62138fa8a8abf531b6b5bbeb37df
Fattah <irfan.fattah.kusuma@student.uty.ac.id>
2019-12-06 00:47:34 +0800

    crud

---
+++ app/src/main/java/com/fatkus/readdb/ApiEndPoint.kt
@@ -0,0 +1,9 @@
+class ApiEndPoint{
+    companion object{
+        private val SERVER = "http://localhost/kotlin/"
+        val CREATE = SERVER+"create_fakultas.php"
+        val READ = SERVER+"read_fakultas.php"
+        val UPDATE = SERVER+"update_fakultas.php"
+        val DELETE = SERVER+"delete_fakultas.php"
+    }
+}
---
+++ app/src/main/java/com/fatkus/readdb/Fakultas.kt
@@ -0,0 +1,7 @@
+package com.fatkus.readdb
+
+data class Fakultas(
+    val id_fakultas: String,
+    val kode_fakultas:String?,
+    val nama_fakultas:String?
+)
